8c4c15a2a7bef7bab91694acf5adea66336ab1e0
fuzzy69 <zeewolf@neobee.net>
2019-11-21 09:02:19 +0100

    Update API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -30,19 +30,38 @@ class TestApi(TestCase):
     def tearDown(self):
         pass
 
-    def test_token(self):
-        """Tests auth header token"""
-        # Invalid token
+    def test_invalid_token(self):
+        """Tests invalid auth header token"""
         self.app.environ_base["HTTP_AUTHORIZATION"] = "Bearer demo:ZGVtb3Rva2Vf"
-        r = self.app.get("/api", follow_redirects=True)
-        json_data = loads(r.data)
+        r = self.app.get("/api/oxts", follow_redirects=True)
         self.assertEqual(r.status_code, 401)
-        # Valid token
+
+    def test_valid_token(self):
+        """Tests valid auth header token"""
         self.app.environ_base["HTTP_AUTHORIZATION"] = "Bearer demo:ZGVtb3Rva2Vu"
-        r = self.app.get("/api", follow_redirects=True)
-        json_data = loads(r.data)
+        r = self.app.get("/api/oxts", follow_redirects=True)
+        json_data = None
+        try:
+            json_data = loads(r.data)
+        except:
+            pass
+        self.assertIsInstance(json_data, dict, "Invalid response type!")
         self.assertEqual(r.status_code, 200)
-        self.assertTrue(json_data.get("status"))
+
+    def test_valid_response_data(self):
+        """Tests valid auth header token"""
+        self.app.environ_base["HTTP_AUTHORIZATION"] = "Bearer demo:ZGVtb3Rva2Vu"
+        r = self.app.get("/api/oxts/2", follow_redirects=True)
+        json_data = None
+        try:
+            json_data = loads(r.data)
+        except:
+            pass
+        self.assertEqual(r.status_code, 200)
+        self.assertIsInstance(json_data, dict, "Invalid response type!")
+        self.assertTrue("data" in json_data)
+        self.assertTrue("oxts" in json_data["data"])
+        self.assertIsInstance(json_data["data"]["oxts"], list, "Invalid reponse data!")
 
 
 if __name__ == "__main__":
