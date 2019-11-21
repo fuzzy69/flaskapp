d81115a3a35c72b03638cccb2217ed4632a955b6
fuzzy69 <zeewolf@neobee.net>
2019-11-21 21:33:39 +0100

    Refactor backend H5 read/query code

--- application/backend.py
+++ application/backend.py
@@ -1,16 +1,14 @@
 # -*- coding: UTF-8 -*-
 
-from os.path import isfile
-from itertools import islice
-
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, current_app, render_template, flash
 from flask_login import login_required
-from flask_paginate import Pagination, get_page_parameter
-import tables
+from flask_paginate import Pagination
 
+from application.data import DataError, DataTable
 from config import IPP, H5_FILE, OXTS_TABLE_COLUMNS
 
 
+app = current_app
 back = Blueprint("back", __name__, url_prefix="/back", template_folder="templates/backend")
 
 
@@ -20,7 +18,8 @@ back = Blueprint("back", __name__, url_prefix="/back", template_folder="template
 def _index():
     """Backend home page"""
     return render_template(
-        "index.html"
+        "index.html",
+        title="Dummy Dashboard page"
     )
 
 @back.route("/oxts", defaults={"page": 1})
@@ -32,27 +31,15 @@ def _oxts(page: int):
     pagination = None
     start = (page - 1) * IPP
     stop = start + IPP
-    if isfile(H5_FILE):
-        h5file = None
-        try:
-            h5file = tables.open_file(H5_FILE, mode="r")
-            table = h5file.root.sequences.oxts
-            total_rows = table.shape[0]
-            rows = [tuple(row[attr].decode("utf-8") if attr == "timestamp" else row[attr]
-                        for attr in OXTS_TABLE_COLUMNS)
-                            for row in table.iterrows(start, stop)]
-            pagination = Pagination(page=page, total=total_rows, per_page=IPP, bs_version=4)
-        except:
-            msg = "Failed to query KITTI data file!"
-            flash(msg, "danger")
-            # app.logger.warning(msg, exc_info=True)
-        finally:
-            if h5file is not None:
-                h5file.close()
-    else:
-        msg = "Failed to locate KITTI data file!"
-        flash(msg, "danger")
-        # app.logger.warning(msg, exc_info=True)
+    try:
+        dt = DataTable(H5_FILE)
+        filters = {}
+        rows = [row for row in dt.fetch_rows_as_tuples(start, stop, **filters)]
+        pagination = Pagination(page=page, total=dt.rows_count, per_page=IPP, bs_version=4)
+    except (DataError, Exception):
+        message = "Failed to query OXTS data!"
+        flash(message, "danger")
+        app.logger.warning(message, exc_info=True)
 
     return render_template(
         "oxts.html",
@@ -73,23 +60,16 @@ def _velo(page: int):
     pagination = None
     start = (page - 1) * IPP
     stop = start + IPP
-    if isfile(H5_FILE):
-        h5file = None
-        try:
-            h5file = tables.open_file(H5_FILE, mode="r")
-            table = h5file.root.sequences.oxts
-            total_rows = table.shape[0]
-            rows = [tuple(row[attr] for attr in OXTS_TABLE_COLUMNS) for row in table.iterrows(start, stop)]
-            pagination = Pagination(page=page, total=total_rows, per_page=IPP, bs_version=4)
-        except:
-            pass
-        finally:
-            if h5file is not None:
-                h5file.close()
-    else:
-        msg = "Failed to locate KITTI data file!"
-        flash(msg, "danger")
-        # app.logger.warning(msg, exc_info=True)
+    # TODO: Replace dummy OXTS data with real Velodyne data
+    try:
+        dt = DataTable(H5_FILE)
+        filters = {}
+        rows = [row for row in dt.fetch_rows_as_tuples(start, stop, **filters)]
+        pagination = Pagination(page=page, total=dt.rows_count, per_page=IPP, bs_version=4)
+    except (DataError, Exception):
+        message = "Failed to query Velodyne data!"
+        flash(message, "danger")
+        app.logger.warning(message, exc_info=True)
 
     return render_template(
         "oxts.html",
